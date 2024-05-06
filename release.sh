a099fc1c6c67d82ff09d72168cff2eb1bf9b31e5
Raya Mahardika <rayapkun@gmail.com>
2024-05-06 14:29:07 +0700

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,31 +1,14 @@
 #!/bin/bash
 
-# Set Github Release version name from names of file
-version=$(ls PixelOS*.zip | tr -d '\n')
+# Copy zip here
+gh auth login
+filename=$(ls *.zip)
 
-# Check if the tag already exists
-if git rev-parse "$version" &> /dev/null; then
-  echo "Error: The release tag '$version' already exists."
-  exit 1
-fi
+# Create a tag and release using the filename (without .zip extension)
+version=${filename%.zip}
 
-# Create the tag and push it to GitHub
 git tag -a "$version" -m "Release $version"
+
 git push origin "$version"
 
-# Upload all .zip files in the current directory
-filenames=(*.zip)
-
-# Create the release on GitHub
-if ! gh release create "$version" --title "Release $version" --notes "Release notes"; then
-  echo "Error: Failed to create the release."
-  exit 1
-fi
-
-# Upload the files to the release
-for filename in "${filenames[@]}"; do
-  gh release upload "$version" "$filename" --clobber
-done
-
-# Display success message
-echo "Files uploaded successfully."
+gh release create "$version" "$filename" -t "Release $version" -n "Release notes"
